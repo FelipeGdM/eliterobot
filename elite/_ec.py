0d62c734725edbeba1f459bfb02f96928b8b43a2
Felipe Gomes de Melo <felipegmelo@usp.br>
2025-09-03 15:08:44 -0300

    Translate comments to english

--- elite/_ec.py
+++ elite/_ec.py
@@ -14,7 +14,7 @@ from elite._var import ECIO as __ECIO
 from elite._var import ECVar as __ECVar
 
 __recommended_min_robot_version = "3.0.0"
-# 所有的接口在v3.0.0进行测试，多数接口在低于该版本时也可以运行，但是并未进行测试
+# All interfaces were tested in v3.0.0. Most interfaces can also run in versions lower than this, but they have not been tested
 
 
 class _EC(
@@ -29,7 +29,7 @@ class _EC(
     __ECMonitor,
     __ECIO,
 ):
-    """EC机器人类,该类实现所有的sdk接口以及自定义的方法"""
+    """EC robot class, implements all SDK interfaces and custom methods"""
 
     def __init__(
         self,
@@ -37,13 +37,13 @@ class _EC(
         name: Optional[str] = "None",
         auto_connect: bool = False,
     ) -> None:
-        """初始化EC机器人
+        """Initialize EC robot
 
         Args
         ----
-            ip (str, optional): 机器人的ip. Defaults to "192.168.1.200".
-            name (Optional[str], optional): 机器人的名字,在打印实例时可以看到. Defaults to "None".
-            auto_connect (bool, optional): 是否自动连接机器人. Defaults to False.
+            ip (str, optional): Robot IP. Defaults to "192.168.1.200".
+            name (Optional[str], optional): Robot name, visible when printing the instance. Defaults to "None".
+            auto_connect (bool, optional): Whether to automatically connect to the robot. Defaults to False.
         """
         super().__init__()
         self.robot_ip = ip
@@ -65,7 +65,7 @@ class _EC(
             return "Elite EC__, IP:%s, Name:%s" % (self.robot_ip, self.robot_name)
 
     def wait_stop(self) -> None:
-        """等待机器人运动停止"""
+        """Wait for the robot motion to stop"""
         while True:
             time.sleep(0.005)
             result = self.state
@@ -73,43 +73,43 @@ class _EC(
                 if result != self.RobotState.STOP:
                     str_ = [
                         "",
-                        "state of robot in the pause",
-                        "state of robot in the emergency stop",
+                        "Robot is in pause state",
+                        "Robot is in emergency stop state",
                         "",
-                        "state of robot in the error",
-                        "state of robot in the collision",
+                        "Robot is in error state",
+                        "Robot is in collision state",
                     ]
                     self.logger.debug(str_[result.value])
                     break
                 break
         self.logger.info("The robot has stopped")
 
-    # 自定义方法实现
+    # Custom method implementation
     def robot_servo_on(self) -> bool:
-        """自动上伺服,绝大数情况都是成功的"""
-        # 对透传状态进行处理
+        """Automatically servo on, successful in the vast majority of cases"""
+        # Handle pass-through state
         if self.TT_state:
             self.logger.debug(
-                "The TT state is enabled, and the TT cache is automatically cleared"
+                "TT state is enabled, automatically clearing TT cache"
             )
             time.sleep(0.5)
             if self.TT_clear_buff():
-                self.logger.debug("The TT cache has been cleared")
+                self.logger.debug("TT cache cleared")
 
         state_str = [
-            "please set Robot Mode to remote",
+            "Please set Robot Mode to remote",
             "Alarm clear failed",
             "MotorStatus sync failed",
-            "servo status set failed",
+            "Servo status set failed",
         ]
         state = 0
         robot_mode = self.mode.value
         if str(robot_mode) == "2":
             state = 1
-            # 清除报警
+            # Clear alarm
             if state == 1:
                 clear_num = 0
-                # 循环清除报警,排除异常情况
+                # Loop to clear alarm, excluding abnormal conditions
                 while 1:
                     self.clear_alarm()
                     time.sleep(0.2)
@@ -119,67 +119,67 @@ class _EC(
                     clear_num += 1
                     if clear_num > 4:
                         self.logger.error(
-                            "The Alarm can't clear,Please check the robot state"
+                            "Alarm cannot be cleared, please check robot state"
                         )
                         return False
-                self.logger.debug("Alarm clear success")
+                self.logger.debug("Alarm cleared successfully")
                 time.sleep(0.2)
-                # 编码器同步
+                # Encoder synchronization
                 if state == 2 and not self.sync_status:
                     if self.sync():
                         state = 3
-                        self.logger.debug("MotorStatus sync success")
+                        self.logger.debug("MotorStatus synchronized successfully")
                         time.sleep(0.2)
-                        # 循环上伺服
+                        # Loop to servo on
                         while 1:
                             self.set_servo_status()
                             if self.servo_status == True:
-                                self.logger.debug("servo status set success")
+                                self.logger.debug("Servo status set successfully")
                                 return True
                             time.sleep(0.02)
                 else:
                     state = 3
-                    self.logger.debug("MotorStatus sync success")
+                    self.logger.debug("MotorStatus synchronized successfully")
                     time.sleep(0.2)
-                    # 上伺服
+                    # Servo on
                     if self.set_servo_status():
-                        # 循环上伺服
+                        # Loop to servo on
                         while 1:
                             self.set_servo_status()
                             if self.servo_status == True:
-                                self.logger.debug("servo status set success")
+                                self.logger.debug("Servo status set successfully")
                                 return True
                             time.sleep(0.02)
         self.logger.error(state_str[state])
         return False
 
     def monitor_thread_run(self):
-        """运行8056数据监控线程
+        """Run 8056 data monitoring thread
 
         Examples
         --------
-        创建实例
+        Create instance
         >>> ec = EC(ip="192.168.1.200", auto_connect=True)
 
-        监控线程运行
+        Start monitoring thread
         >>> ec.monitor_thread_run()
 
-        当该方法执行后,可以通过以下方法进行查看监控的数据
+        After executing this method, monitored data can be viewed using:
         >>> while 1:
         >>>     ec.monitor_info_print()
         >>>     time.sleep(1)
 
-        以上方法即会在控制台打印数据
+        The above method will print data to the console
         """
         self.monitor_thread = threading.Thread(
             target=self.monitor_run,
             args=(),
             daemon=True,
-            name="Elibot monitor thread,IP:%s" % (self.robot_ip),
+            name="Elibot monitor thread, IP:%s" % (self.robot_ip),
         )
         self.monitor_thread.start()
 
     def monitor_thread_stop(self):
-        """停止8056数据监控线程"""
+        """Stop 8056 data monitoring thread"""
         self.monitor_run_state = False
         self.monitor_thread.join()
